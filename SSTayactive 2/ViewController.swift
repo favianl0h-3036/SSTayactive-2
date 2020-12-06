34dc6eaec702d19dcc6ce3a95159850322daf154
Chew Caven <chew_caven@s2019.ssts.edu.sg>
2020-12-06 21:22:44 +0800

    Initial Commit

---
+++ SSTayactive 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SSTayactive 2
+//
+//  Created by Chew Caven on 6/12/20.
+//  Copyright © 2020 Electron Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
